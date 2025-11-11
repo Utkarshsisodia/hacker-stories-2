b45006854d5b0bf1088b140e8dbf1032f30f42c4
Utkarshsisodia <usisodia67@gmail.com>
2025-11-11 22:42:57 +0530

    Fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,26 @@ import "./App.css";
 import List from "./components/List";
 import InputWithLabel from "./components/InputWithLabel";
 import Title from "./components/Title";
-const initialList = [
-  {
-    title: "React",
-    url: "https://react.dev/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-const getAsyncStories = () =>
-  new Promise((resolve) =>
-    setTimeout(
-      () => resolve({ data: { stories: initialList} }),
-      2000
-    )
-  );
-  
-  const storiesReducer = (state, action) => {
+//   {
+//     title: "React",
+//     url: "https://react.dev/",
+//     author: "Jordan Walke",
+//     num_comments: 3,
+//     points: 4,
+//     objectID: 0,
+//   },
+//   {
+//     title: "Redux",
+//     url: "https://redux.js.org/",
+//     author: "Dan Abramov, Andrew Clark",
+//     num_comments: 2,
+//     points: 5,
+//     objectID: 1,
+//   },
+// ];
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
+ 
+const storiesReducer = (state, action) => {
     switch (action.type) {
       case 'STORIES_FETCH_INIT':
         return {...state, isLoading : true, isError : false,};
@@ -55,11 +48,11 @@ function App() {
   useEffect(() => {
     dispatchStories({type : 'STORIES_FETCH_INIT'});
     
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}React`).then((response) => response.json())
     .then(result => {
       dispatchStories({
         type : 'STORIES_FETCH_SUCCESS',
-        payload : result.data.stories,
+        payload : result.hits,
       })
       
     })
